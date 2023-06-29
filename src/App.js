518d9f6b6390f7005adf140f092bfbefb99bac50
Vijay <vijaysadhuram4868@gmail.com>
2023-06-29 16:13:30 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import Header from './Components/Header'
 import './Components/Header.css'
 import Home from './Components/Home'
 import './App.css'
-import {BrowserRouter} from "react-router-dom"
+import About from './Components/About'
+import Contact  from './Components/Contact'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Footer  from './Components/Footer'
 function App() {
   return (
    < BrowserRouter>
-    <div>
+     
+     <Header/>
+     <Routes>
+ 
+     <Route path='/' element={<Home/>}/>
 
-    <Header/>
-     <Home/>
-    </div>
+     <Route path='/about' element={<About/>}/>
+     <Route path='/contact' element={<Contact/>}/>
+
+     </Routes>
+
+
+    <Footer/>
+     {/* <Home/>
+     <About/>
+     <Contact/>
+      */}
+    
     </BrowserRouter>
   )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
+    <>
     <nav>Get Ready to complete your Goal</nav>
+    <div className='header'>
+     
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+    </div>
+
+    </>
   )
 }
 
